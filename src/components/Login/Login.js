5a3040f2207937c84c18b1834b273f1dc8594c2a
aakritichapain <akriteechapain@gmail.com>
2020-01-29 21:28:56 +0545

    user registered using API

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,136 +1,108 @@
-import React from "react";
-import MDBreact from "mdbreact";
-import ReactDOM from 'react-dom'
-import 'mdbreact/dist/css/mdb.css';
-import './Login.css'
-
-import { Container, Row, Col, Card, CardBody, Button, Label, Input, Icon, ModalFooter } from 'mdbreact';
-import { Link, Redirect } from 'react-router-dom'
-class Login extends React.Component{
+import React from 'react'
+import { 
+  Form, Button
+} from 'react-bootstrap'
+import Axios from 'axios';
+import 'mdbreact/dist/css/mdb.css'
+import { Container,MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody, MDBView,ModalFooter} from 'mdbreact';
+import { Redirect,Link } from 'react-router-dom';
+class Registration extends React.Component {
 
 constructor(){
   super()
+
   this.state = {
-
-    email:'',
-    password:'',
-    color: "#000000"
-
+    
 
   }
 }
 
-emailChangeHandler = (event) => {
-
-  this.setState({email: event.target.value})
 
 
-}
-
-passwordChangeHandler = (event) => {
-
-this.setState({password: event.target.value})
-  
-}
-
-fromSubmitHnadler = (e) => {
-  e.preventDefault()
-
-
-// use API call to post the data 
-//fetch byt default JS
-// Axios external package
-
-
-  console.log(this.state) // this sate js object
-  // 1st url 
-  // 2nd data JS object
-  // 3rd header JS object 
-var headers = {
-
-'Content-Type':'application/json'
-
-}
-  Axios.post('http://localhost:3023/login', this.state , headers)
-  .then(function(response){
-
-  })
-  .catch(function(err){
-
-  })
-
-}
 
 render(){
+
+//what to render based in state
+
 if(this.state.redirect){
 
 return (
-  <Redirect to='/registration' />
+  <Redirect to='/home' />
   )
-}
-  
-  return (
-   
 
-    <Container>
-     
-    <Row >
-        <Col md="2" style={{marginRight:'800px',height:'500px', marginTop:'15px'}} >
+// toast message
+
+}
+
+
+
+  return(
+
+<Container>
+<div>
+ <MDBContainer>
+      <MDBRow>
+      <MDBCol md="2" style={{marginRight:'800px',height:'500px', marginTop:'15px'}} >
         <img src= "./Image/s.jpg" />
-        </Col>
+        </MDBCol>
     
-        <Col md="5" style={{marginLeft:'-350px',marginTop:'15px'}} >
-          <Card style={{marginTop:'-1px',height:'428px'}}>
-            <CardBody className="mx-6">
-              <div className="text-center">
-                <h3 className="dark-grey-text mb-5">
-                  <strong >Login</strong>
-                </h3>
-              </div>
-              <Input 
-                label="Your email"
-                group
-                type="email" name='email' id='email'
-                validate
-                error="wrong"
-                success="right" value={this.state.email} onChange={this.emailChangeHandler} 
-              />
-              <Input
-                label="Your password"
-                group
-                type="password" name='password' id='password'
-                validate
-                containerClass="mb-0"  value={this.state.password} onChange={this.passwordChangeHandler}
-              />
-              
-              <div className="text-center mb-3">
-                <Button
-                  type="button"
+        <MDBCol md="5" style={{marginLeft:'-350px',marginTop:'15px'}} >
+          <MDBCard style={{marginTop:'-1px',height:'428px'}}>
+            <MDBCardBody className="mx-6">
+              <form  >
+                <p className="h4 text-center py-4">Login</p>
+                <div className="grey-text">
+
+
+                  <MDBInput
+                    label="Email" 
+                    icon="envolope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  
+                  <MDBInput
+                    label="Password" 
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                
+                </div>
+                <div className="text-center py-6 mt-6">
+                 <MDBBtn
+                  type="submit"
                   gradient="blue"
                   rounded
                   className="btn-block z-depth-1a"
-                  onClick={this.handleSubmit}
-                >
-                   Login
-                </Button>
-              </div>
-            </CardBody>
-            <ModalFooter className="mx-5 pt-3 mb-1">
+                 >
+                  Login
+                </MDBBtn>
+                </div>
+              </form>
+            </MDBCardBody>
+             <ModalFooter className="mx-5 pt-3 mb-1">
               <p className="font-small grey-text d-flex justify-content-end">
                 Not a member?
-                 <Link to='/' className="blue-text ml-1">Sign Up Here! </Link> 
+                 <Link to='/registration' className="blue-text ml-1">Sign Up Here! </Link> 
 
                
                 
               </p>
             </ModalFooter>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
- 
-  )
-}
-}
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
 
-export default Login;
+    
+      </div>
+      </Container>
+  );
+}};
+
+export default Registration;
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import MDBreact from "mdbreact";
-import ReactDOM from 'react-dom'
-import 'mdbreact/dist/css/mdb.css';
-import "./Registration.css"
-
-import { Container, Row, Col, Card, CardBody, Button, Label, Input, Icon, ModalFooter } from 'mdbreact';
-import { Link, Redirect } from 'react-router-dom'
+import { 
+  Form, Button
+} from 'react-bootstrap'
 import Axios from 'axios';
+import 'mdbreact/dist/css/mdb.css'
+import { Container,MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody, MDBView} from 'mdbreact';
+import { Redirect } from 'react-router-dom';
 class Registration extends React.Component {
 
 constructor(){
   super()
-  
 
   this.state = {
+
+    
     fullname:'',
     address:'',
     phone:'',
     email:'',
     password:'',
     validationMessage:'',
-     color: "#9ebd4b",
     redirect:false
 
   }
 }
+
 
 fullnameChangeHandler = (event) => {
 
@@ -38,19 +38,17 @@ this.setState({address: event.target.value})
 }
 phoneChangeHandler = (event) => {
 
-this.setState({phone: event.target.value})
-  
+
+  this.setState({phone: event.target.value})
 }
+
 emailChangeHandler = (event) => {
 
-if(event.target.value.length < 6){
+/*if(event.target.value.length < 6){
   this.setState({validationMessage:'Username Cannot be less than 6 chars '})
-}
+}*/
 
-  this.setState({username: event.target.value})
-
-
-
+  this.setState({email: event.target.value})
 }
 
 passwordChangeHandler = (event) => {
@@ -76,6 +74,7 @@ var headers = {
 
 var data = {
 
+  
   fullname:this.state.fullname,
   address:this.state.address,
   phone:this.state.phone,
@@ -85,7 +84,7 @@ var data = {
 }
 
 //mfetch method XMLHTTPREquest
-  Axios.post('http://localhost:3023/registration', data , headers)
+  Axios.post('http://localhost:3000/users/signup', data , headers)
 
 .then( (response) => {
   console.log(response.data.status);
@@ -125,94 +124,96 @@ return (
 
 
   return(
-  <Container>
-     
-    <Row >
-       {/* <Col md="2" style={{marginRight:'800px',height:'500px'}} >
-        <img src= "./Image/s.jpg" />
-        </Col>*/}
-    
-        <Col md="6" style={{marginRight:'100px'}} >
-          <Card style={{marginTop:'opx'}}>
-            <CardBody className="mx-6">
-              <div className="text-center">
-                <h3 className="dark-grey-text mb-5">
-                  <strong >NEW ACCOUNT?</strong>
+
+//if(this.state.redirect == true){ 
+
+//   //actual redirect work
+
+// }
+<Container>
+<div>
+ <MDBContainer style={{
+  paddingLeft:"20px"}}>
+      <MDBRow>
+        <MDBCol md="6">
+          <MDBCard>
+            <MDBCardBody>
+              <form onSubmit={this.formSubmitHandler} >
+                <p className="h4 text-center py-4">NEW ACCOUNT?</p>
                   <h6>Create one for FREE here and become a Grocery member.</h6>
-                </h3>
-              </div>
-               <Input 
-                label="Fullname"
-                group
-                type="text" name='fullname' id='fullname'
-                validate
-                error="wrong"
-                success="right" value={this.state.fullname} onChange={this.fullnameChangeHandler} 
-              />
-               <Input 
-                label="Address"
-                group
-                type="text" name='address' id='address'
-                validate
-                error="wrong"
-                success="right" value={this.state.address} onChange={this.addressChangeHandler} 
-              />
-               <Input 
-                label="Phone Number"
-                group
-                type="number" name='phone' id='phone'
-                validate
-                error="wrong"
-                success="right" value={this.state.phone} onChange={this.phoneChangeHandler} 
-              />
-              <Input 
-                label="Email"
-                group
-                type="email" name='email' id='email'
-                validate
-                error="wrong"
-                success="right" value={this.state.email} onChange={this.emailChangeHandler} 
-              />
-              <Input
-                label="Password"
-                group
-                type="password" name='password' id='password'
-                validate
-                containerClass="mb-0"  value={this.state.password} onChange={this.passwordChangeHandler}
-              />
-              
-              <div className="text-center mb-3">
-                <Button
-                  type="button"
-                  gradient="dusty-grass"
+                <div className="grey-text">
+                
+                  <MDBInput 
+                    label=" Fullname" value={this.state.fullname} onChange={this.fullnameChangeHandler}
+                    icon="user"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Address" value={this.state.address} onChange={this.addressChangeHandler}
+                    icon="home"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Phone Number" value={this.state.phone} onChange={this.phoneChangeHandler}
+                    icon="envolope"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Email" value={this.state.email} onChange={this.emailChangeHandler}
+                    icon="envolope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  
+                  <MDBInput
+                    label="Password" value={this.state.password} onChange={this.passwordChangeHandler}
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                
+                </div>
+                <div className="text-center py-6 mt-6">
+                 <MDBBtn
+                  type="submit"
+                  gradient="blue"
                   rounded
                   className="btn-block z-depth-1a"
-                  onClick={this.handleSubmit}
-                  
-                >
+                 >
                   Register
-                </Button>
-              </div>
-            </CardBody>
-            <ModalFooter className="mx-5 pt-3 mb-1">
-              <p className="font-small grey-text d-flex justify-content-end">
-                Already a member?
-                 <Link to='/' className="dusty-grass-text ml-1">Login Here! </Link> 
+                </MDBBtn>
+                </div>
+              </form>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
 
-               
-                
-              </p>
-            </ModalFooter>
-          </Card>
-        </Col>
-
-        <Col md="2" style={{marginLeft:'-5px', marginTop:'50px'}} >
+         <MDBCol md="2" style={{marginLeft:'-5px', marginTop:'50px'}} >
         <img src= "./Image/a.png" />
-        </Col>
-      </Row>
-    </Container>
-  )
-}
-}
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
 
-export default Registration
+    
+      </div>
+      </Container>
+  );
+}};
+
+export default Registration;
